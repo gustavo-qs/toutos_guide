db7248e8b0f4d7a66891597987dedab95b67e3a0
Gustavo Queiroz <compgustavo@edu.unifil.br>
2024-10-14 20:08:12 -0300

    first commit, adding tailwind

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="rounded">
+      <h1 class=" text-4xl font-bold text-center text-gray-900">Toutos Guide to Run a Encounter</h1>
+    </div>
+  </main>
+</template>
