5ad2718754ab412d17aa7651567d51cbb5cfa345
Gustavo Queiroz <gustavo.qs2103@gmail.com>
2025-09-28 23:42:40 -0300

    please work this should be simple

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: 'https://gustavo-qs.github.io/toutos_guide/'
+  base: '/toutos_guide/'
 })
